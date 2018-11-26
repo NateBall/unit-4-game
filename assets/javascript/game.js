edee385a31161a6229f363379963a08b2f804fa6
Gobdomino <nateball13@gmail.com>
2018-11-26 06:26:50 -0500

    sunday additions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,63 @@
+// Global Variables
+// ===========================================================
+
+
+// Math 
+var userGuess = 0;
+var compGuess = 0;
+var totWins = 0;
+var totLosses = 0;
+var savedNum = Math.floor(Math.random() * 12) + 1;
+
+// Gems
+var pinkGem = 0;
+var blueGem = 0;
+var yellowGem = 0;
+var greenGem = 0;
+
+
+// Reset
+// ========================================================= 
+$("reset").click(function() {
+
+    });
 
 
 
-// Code for random max min with function
-<button onclick="document.getElementById('demo').innerHTML = getRndInteger(19,120)">Click Me</button>
+// Process
+// =========================================================
+startPlay ();
+
+
+
+// Functions
+// ========================================================
+$(document).ready(function(){
+
+    // jQuery methods go here...
+    $("button").click(function(){
+        function getRndInteger(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+    });
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+
+
+// Saves the number until reset is pressed
+$(".colGem").click(function() {
+    $("#scoreNum").html(savedNum);
+  });
+
+// Code for random max min with function
+<button onclick="document.getElementById('compGuess').innerHTML = getRndInteger(19,120)">Click Me</button>
+
+<button onclick="document.getElementById('pinkGem blueGem yellowGem greenGem').innerHTML = getRndInteger(1,12)"></button>
+
+
 
 
 
