2799051f3d929fb08a9ccbb5d670448da8ce4891
Gobdomino <nateball13@gmail.com>
2018-11-28 19:24:44 -0500

    added userGuess and compGuess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,58 +4,69 @@
 
 // Math 
 var userGuess = 0;
-var compGuess = 0;
+var compGuess = Math.floor(Math.random() * 101) + 19;;
 var totWins = 0;
 var totLosses = 0;
-var savedNum = Math.floor(Math.random() * 12) + 1;
+
 
 // Gems
-var pinkGem = 0;
-var blueGem = 0;
-var yellowGem = 0;
-var greenGem = 0;
-
-
-// Reset
-// ========================================================= 
-$("reset").click(function() {
-
-    });
-
-
-
-// Process
-// =========================================================
-startPlay ();
-
+var pinkGem = Math.floor(Math.random() * 12) + 1;
+var blueGem = Math.floor(Math.random() * 12) + 1;
+var yellowGem = Math.floor(Math.random() * 12) + 1;
+var greenGem = Math.floor(Math.random() * 12) + 1;
 
 
 // Functions
 // ========================================================
 $(document).ready(function(){
+    // Computer's Guess
+    $("#compGuess").text(compGuess);
 
-    // jQuery methods go here...
-    $("button").click(function(){
-        function getRndInteger(min, max) {
-            return Math.floor(Math.random() * (max - min)) + min;
-        }
-    });
+    // buttons for gems
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+    $('#pinkGem').on('click', function(){
+        userGuess = userGuess + pinkGem;
+        console.log("New userGuess= " + userGuess);
+        $('#userGuess').text(userGuess);
+    }); 
+
+    $('#blueGem').on('click', function(){
+        userGuess = userGuess + blueGem;
+        console.log("New userGuess= " + userGuess);
+        $('#userGuess').text(userGuess);
+    }); 
+
+    $('#yellowGem').on('click', function(){
+        userGuess = userGuess + yellowGem;
+        console.log("New userGuess= " + userGuess);
+        $('#userGuess').text(userGuess);
+    }); 
+
+    $('#greenGem').on('click', function(){
+        userGuess = userGuess + greenGem;
+        console.log("New userGuess= " + userGuess);
+        $('#userGuess').text(userGuess);
+    }); 
+
+    // Compares User versus Computer Score
+    if (userGuess == compGuess){
+        yay();
+      }
+      else if ( userGuess > compGuess){
+        loser();
+      } 
+    
+   
+
+    // Reset
+    
+});
 
 
 
-// Saves the number until reset is pressed
-$(".colGem").click(function() {
-    $("#scoreNum").html(savedNum);
-  });
 
-// Code for random max min with function
-<button onclick="document.getElementById('compGuess').innerHTML = getRndInteger(19,120)">Click Me</button>
 
-<button onclick="document.getElementById('pinkGem blueGem yellowGem greenGem').innerHTML = getRndInteger(1,12)"></button>
+
 
 
 
