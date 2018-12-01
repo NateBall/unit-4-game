b4f41eb75208bfac8ecb3e5e1a463bd5bbb467d8
Gobdomino <nateball13@gmail.com>
2018-12-01 09:52:50 -0500

    final project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@
 
 // Math 
 var userGuess = 0;
-var compGuess = Math.floor(Math.random() * 101) + 19;;
+var compGuess = Math.floor(Math.random() * 101) + 19;
 var totWins = 0;
 var totLosses = 0;
 
@@ -18,51 +18,77 @@ var greenGem = Math.floor(Math.random() * 12) + 1;
 
 // Functions
 // ========================================================
-$(document).ready(function(){
+$(document).ready(function () {
     // Computer's Guess
-    $("#compGuess").text(compGuess);
+    $("#compGuess").html(compGuess);
 
     // buttons for gems
 
-    $('#pinkGem').on('click', function(){
+    $('#pinkGem').on('click', function () {
         userGuess = userGuess + pinkGem;
         console.log("New userGuess= " + userGuess);
-        $('#userGuess').text(userGuess);
-    }); 
+        $('#userGuess').html(userGuess);
+        winLoss();
+    });
 
-    $('#blueGem').on('click', function(){
+    $('#blueGem').on('click', function () {
         userGuess = userGuess + blueGem;
         console.log("New userGuess= " + userGuess);
-        $('#userGuess').text(userGuess);
-    }); 
+        $('#userGuess').html(userGuess);
+        winLoss();
+    });
 
-    $('#yellowGem').on('click', function(){
+    $('#yellowGem').on('click', function () {
         userGuess = userGuess + yellowGem;
         console.log("New userGuess= " + userGuess);
-        $('#userGuess').text(userGuess);
-    }); 
+        $('#userGuess').html(userGuess);
+        winLoss();
+    });
 
-    $('#greenGem').on('click', function(){
+    $('#greenGem').on('click', function () {
         userGuess = userGuess + greenGem;
         console.log("New userGuess= " + userGuess);
-        $('#userGuess').text(userGuess);
-    }); 
+        $('#userGuess').html(userGuess);
+        winLoss();
+    });
 
     // Compares User versus Computer Score
-    if (userGuess == compGuess){
-        yay();
-      }
-      else if ( userGuess > compGuess){
-        loser();
-      } 
-    
-   
+    var winLoss = function() {
+        if (userGuess === compGuess) {
+            alert("Yay! You win!");
+            // tot wins increase
+            totWins++;
+            $(".totalWins").html(totWins);
+        }
+        else if (userGuess > compGuess) {
+            alert("Sorry! Try Again!");
+            // tot losses increase
+            totLosses++;
+            $(".totalLosses").html(totLosses);
+        }
+    };
 
-    // Reset
-    
+
+
 });
 
+// Reset
+// ===================================================
+var reset = function() {
+    userGuess = 0;
+    compGuess = Math.floor(Math.random() * 101) + 19;
+    pinkGem = Math.floor(Math.random() * 12) + 1;
+    blueGem = Math.floor(Math.random() * 12) + 1;
+    yellowGem = Math.floor(Math.random() * 12) + 1;
+    greenGem = Math.floor(Math.random() * 12) + 1;
+};
 
+$('.playAgain').on('click', function () {
+    console.log(compGuess, pinkGem);
+    reset();
+    $("#compGuess").text(compGuess);
+    $('#userGuess').text("");
+});
 
 
 
